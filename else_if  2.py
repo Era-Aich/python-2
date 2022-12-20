ea7353927456d1916d0bfd698c0d8285d193166a
eraaich488@gmail.com <eraaich488@gmail.com>
2022-12-20 10:23:06 +0600

    first commit

---
+++ else_if  2.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 20 09:59:'00 2022
+
+@author: co'mputer market
+"""
+
+a=10
+b=9
+
+if(a>b):
+    print("a is bigger than b")
+    
+elif(b>a):
+        print("b is bigger than a")
+        
+else:
+            
+    print("Both are euqal")
---
+++ if_condition.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+a=5
+b=4
+c=2
+n = 5
+
+if(a>b or b>c):
+    print(b)
+if(a>c and b>c):
+    print(c)
+else:
+    print("condition false")
+            
+    list=[1,2,6,4,9]
+  
+if n not in list:
+    print("n is not in the list")
